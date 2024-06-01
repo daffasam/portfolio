88d59452e7541f1ad7ef08ca82f235bcc1b95b10
Daffa Sacofi Mutawakkil <daffasacofi359@gmail.com>
2024-06-01 13:13:13 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+});
---
+++ assets/js/typed.js
@@ -0,0 +1,5 @@
+const typed = new Typed(".typing-job", {
+  strings: ["UI/UX Designer", "Frontend Web Developer"],
+  typeSpeed: 100,
+  loop: true,
+});
