a34bd64a3433451af37d813390314880351adb62
Daffa Sacofi Mutawakkil <90497792+daffasam@users.noreply.github.com>
2024-06-06 18:12:38 +0700

    Update typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".typing-job", {
-  strings: ["UI/UX Designer", "Frontend Web Developer"],
+  strings: ["Junior UI/UX Designer", "Junior Frontend Web Developer"],
   typeSpeed: 100,
   loop: true,
 });
