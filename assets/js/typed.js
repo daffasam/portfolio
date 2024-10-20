71d37bb009b6e950c2497bb77ab28fccf1327614
Daffa Sacofi Mutawakkil <daffasacofi359@gmail.com>
2024-10-20 17:10:53 +0700

    change contact

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".typing-job", {
-  strings: ["UI/UX Designer", "Frontend Web Developer"],
+  strings: ["UI/UX Designer", "Frontend Web Developer", "Web Designer"],
   typeSpeed: 100,
   loop: true,
 });
